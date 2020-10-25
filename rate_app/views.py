b3bda3ea8a0133b9a7f3cf1df352966b2ebb4ede
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-25 11:09:55 +0300

    serializers added

--- rate_app/views.py
+++ rate_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializer import *
+from rest_framework import status
+
 
 # Create your views here.
 def index(request):
@@ -6,3 +12,29 @@ def index(request):
 
 def profile(request):
   return render(request,'profile.html')
+
+class ProfileList(APIView):
+  def get(self, request, format=None):
+    profiles = Profile.objects.all()
+    serializers = ProfileSerializer(profiles, many=True)
+    return Response(serializers.data)
+
+  def post(self, request,format=None):
+    serializers = ProfileSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  
+class ProjectList(APIView):
+  def get(self,request,format=None):
+    projects = Project.objects.all()
+    serializers = ProjectSerializer(projects,many=True)
+    return Response(serializers.data)
+  
+  def post(self,request,format=None):
+    serializers=ProjectSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response  
