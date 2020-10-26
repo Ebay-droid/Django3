b29257267b9c22a2ea4988dfeae539864bc96b57
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-26 08:28:45 +0300

    profiile view added

--- rate_app/views.py
+++ rate_app/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
 from .serializer import *
 from rest_framework import status
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html')
 
-def profile(request):
+@login_required
+def profile(request,username):
+  user = get_object_or_404(User,username=username)
+  profile = Profile.objects.get(username=username)
   return render(request,'profile.html')
 
 def project(request):
+  
   return render (request,'projects.html')
 
 def project_detail(request):
