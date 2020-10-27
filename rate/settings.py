b42a46df6106fe50c1782af4f49edb1469a2ff82
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-27 11:17:27 +0300

    static folder added

--- rate/settings.py
+++ rate/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
