ce3afba44e2cf7133f4a3bba74d87baf17f4efaa
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-27 15:44:28 +0300

    Tests  done minus rating

--- rate_app/tests.py
+++ rate_app/tests.py
@@ -16,5 +16,39 @@ class  ProfileTest(TestCase):
 class ProjectTest(TestCase):
    def setUp(self):
      user =User.objects.create(username='test',password='ugali')
-     profile = Profile.objects.create(user=user,Bio='we gat this')   
+     profile = Profile.objects.create(user=user,Bio='we gat this',email='e@gmail.com',phone_number=155151)   
+     self.test = Project(user =user,title='test',description='lets test',project_link='www.test.com',profile=profile)
+     
+  def test_instance(self):
+    self.assertTrue(isinstance(self.test,Project))   
     
+  def test_save(self):
+    self.test.save_project()
+    saved = Project.objects.all()
+    self.assertTrue(len(saved)>0)
+    
+  def test_delete(self):
+    self.test.delete_project()
+    deleted = Project.objects.all()
+    self.assertTrue(len(deleted)== 0) 
+    
+  def tearDown(self):
+    Project.objects.all().delete()
+      
+      
+class ReviewTest(TestCase):
+  
+  def setUp(self):
+   user =User.objects.create(username='test',password='ugali')
+   profile = Profile.objects.create(user=user,Bio='we gat this',email='e@gmail.com',phone_number=155151) 
+   project = Project.objects.create(user=user,title='lets test',description='we can',project_link='www.iwill.com',profile=profile)  
+    
+   self.test = Review (user=user,project=project,review='you actually did it')
+   
+  def test_instance(self):
+    self.assertTrue(isinstance(self.test,Review)) 
+    
+    
+# class RatingTest(TestCase):
+#   def setUp(self):
+        
